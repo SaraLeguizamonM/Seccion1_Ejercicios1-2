37d4f4311d02f0c1b8fb7fe148f3143244cf3e39
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-20 01:34:40 -0500

    Menu con las dos opciones del ejercicio

--- Main.java
+++ Main.java
@@ -4,38 +4,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        do { 
+        do {
             System.out.println("Bienvenido al Convertidor de Unidades Avanzadas ");
-            System.out.println("Le recordamos que solo se permite la conversion entre unidades de la misma categoria ");
-            System.out.println("Categorias disponibles: ");
-            System.out.println("1. Longitud (km, m, cm, mm, µm, mi, yd, ft, in) ");
-            System.out.println("2. Masa (kg, g, mg, t, lb, oz, ton) ");
-            System.out.println("3. Temperatura (C, K, F, R) ");
-            System.out.println("4. Volumen (m3, L, mL, cm3, kL, gal, qt, pt, oz, bbl) ");
-            System.out.println("5. Tiempo (s, min, h, d, a, ms, µs) ");
-            System.out.println("0. Salir ");
-        
-            System.out.print("Seleccione la categoria de unidades que desea convertir (1-5): ");
-            switch (sc.nextLine()) {
-                case "1":
-                    
+            System.out.println("Elige la opcion que desees ejecutar: ");
+            System.out.println("1. Ingresar el valor y la unidad");
+            System.out.println("2. Ingresar solamente el valor");
+            System.out.println("0. Salir");
+            switch(sc.nextInt()) {
+                case 1:
+                    System.out.println(" ");
+                    Medida.unidadValor(sc);
+                    sc.nextLine();
                     break;
-                case "2":   
+                case 2:
+                    System.out.println(" ");
+                    Medida.unidadPorDefecto(sc);
+                    sc.nextLine();
                     break;
-                case "3":
-                    break;
-                case "4":
-                    break;
-                case "5":
-                    break;
-                case "0":
-                    System.out.println("Gracias por usar el Convertidor de Unidades Avanzadas. ¡Hasta luego!");
+                case 0:
                     sc.close();
                     return;
                 default:
-                    
+                    System.out.println("Opcion no reconocida, porfavor intentelo de nuevo");
+                    System.out.println(" ");
             }
-
-        } while (true);
+        }while (true);
     }
 }
