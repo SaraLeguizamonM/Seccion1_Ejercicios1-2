6dd28380025853688cb4e3db68b97cd2060dbe22
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-20 16:28:40 -0500

    Correcion 2, simplificar y uso de metodos
    Se hizo un metodo general para filtrar

--- Medida.java
+++ Medida.java
@@ -7,8 +7,8 @@ public class Medida {
         System.out.println("Le recordamos que solo se permite la conversion entre unidades de la misma categoria ");
         System.out.println("Categorias disponibles: ");
         System.out.println("1. Longitud (km, m, cm, mm, µm, mi, yd, ft, in) ");
-        System.out.println("2. Masa (kg, g, mg, t, lb, oz, ton) ");
-        System.out.println("3. Temperatura (C, K, F, R) ");
+        System.out.println("2. Masa (kg, g, mg, lb, oz, ton) ");
+        System.out.println("3. Temperatura (C, K, F) ");
         System.out.println("4. Volumen (m3, L, mL, cm3, kL, gal, qt, pt, oz, bbl) ");
         System.out.println("5. Tiempo (s, min, h, d, a, ms, µs) ");
         System.out.println("0. Volver al menu principal ");
@@ -18,27 +18,32 @@ public class Medida {
         switch (sc.nextLine()) {
             case "1":
                 System.out.println(" ");
-                Tools.categoriaLongitud("m", valor);
+                Main.unidad = "m";
+                Tools.categoriaLongitud(Main.unidad, valor);
 
                 break;
             case "2":
                 System.out.println(" ");
-                Tools.categoriaMasa("kg", valor);
+                Main.unidad = "kg";
+                Tools.categoriaMasa(Main.unidad, valor);
 
                 break;
             case "3":
                 System.out.println(" ");
-                Tools.categoriaTemperatura("C", valor);
+                Main.unidad = "C";
+                Tools.categoriaTemperatura(Main.unidad, valor);
 
                 break;
             case "4":
                 System.out.println(" ");
-                Tools.categoriaVolumen("L", valor);
+                Main.unidad = "L";
+                Tools.categoriaVolumen(Main.unidad, valor);
                 sc.nextLine();
                 break;
             case "5":
                 System.out.println(" ");
-                Tools.categoriaTiempo("s", valor);
+                Main.unidad = "s";
+                Tools.categoriaTiempo(Main.unidad, valor);
                 sc.nextLine();
                 break;
             case "0":
@@ -54,44 +59,20 @@ public class Medida {
     }
 
     public static void unidadValor(String unidad, double valor) {
-        String[] unidadlongitud = {"km", "m", "cm", "mm", "µm", "mi", "yd", "ft", "in"}; // Nos permite validar desde una base, asi evitamos numeros o caracteres especiales
-        for(int i = 0; i < unidadlongitud.length; i++){
-            if(unidadlongitud[i].equals(unidad)){
-                Tools.categoriaLongitud(unidad, valor);
-                break;
-            }
+        if (Tools.convertirNuevaUnidad(unidad).equals("Longitud")) {
+            Tools.categoriaLongitud(unidad, valor);
         }
-        String[] unidadMasa = {"kg", "g", "mg", "t", "lb", "oz", "ton"}; // Nos permite validar desde una base, asi evitamos numeros o caracteres especiales
-        for(int i = 0; i < unidadMasa.length; i++){
-            if(unidadMasa[i].equals(unidad)){
-                Tools.categoriaMasa(unidad, valor);
-                break;
-            }
+        if (Tools.convertirNuevaUnidad(unidad).equals("Masa")) {
+            Tools.categoriaMasa(unidad, valor);
         }
-        String[] unidadTemperatura = {"C", "K", "F", "R"}; // Nos permite validar desde una base, asi evitamos numeros o caracteres especiales
-        for(int i = 0; i < unidadTemperatura.length; i++){
-            if(unidadTemperatura[i].equals(unidad)){
-                Tools.categoriaTemperatura(unidad, valor);
-                break;
-            }
+        if (Tools.convertirNuevaUnidad(unidad).equals("Temperatura")) {
+            Tools.categoriaTemperatura(unidad, valor);
         }
-        String[] unidadVolumen = {"m3", "L", "mL", "cm3", "kL", "gal", "qt", "pt", "oz", "bbl"}; // Nos permite validar desde una base, asi evitamos numeros o caracteres especiales
-        for(int i = 0; i < unidadVolumen.length; i++){
-            if(unidadVolumen[i].equals(unidad)){
-                Tools.categoriaVolumen(unidad, valor);
-                break;
-            }
+        if (Tools.convertirNuevaUnidad(unidad).equals("Volumen")) {
+            Tools.categoriaVolumen(unidad, valor);
         }
-        String[] unidadTiempo = {"s", "min", "h", "d", "a", "ms", "µs"}; // Nos permite validar desde una base, asi evitamos numeros o caracteres especiales
-        for(int i = 0; i < unidadTiempo.length; i++){
-            if(unidadTiempo[i].equals(unidad)){
-                Tools.categoriaTiempo(unidad, valor);
-                break;
-            }
+        if (Tools.convertirNuevaUnidad(unidad).equals("Tiempo")) {
+            Tools.categoriaTiempo(unidad, valor);
         }
     }
-
-
-
-
 }
