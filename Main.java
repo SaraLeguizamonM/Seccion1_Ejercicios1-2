b8db0a9f175b50ea14786e3026c2a44b58f7c8dc
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-22 01:33:13 -0500

    Correcion 3 main cambio con los bucles
    Se agrega try catch para hacer control de numeros o strings.

--- Main.java
+++ Main.java
@@ -2,46 +2,61 @@ import java.util.Scanner;
 
 public class Main {
     public static String unidad;
-    public static double valor;
+    public static String valor;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        do {
-            System.out.println("Bienvenido al Convertidor de Unidades Avanzadas ");
-            System.out.println("Elige la opcion que desees ejecutar: ");
+        System.out.println("Bienvenido al Convertidor de Unidades Avanzadas ");
+        System.out.println("Elige la opcion que desees ejecutar: ");
+        String opcion = " ";
+        while (!opcion.equals("2") || !opcion.equals("0") || !opcion.equals("1")) {
             System.out.println("1. Ingresar el valor y la unidad");
             System.out.println("2. Ingresar solamente el valor");
             System.out.println("0. Salir");
             System.out.println(" ");
-            String opcion = "";
-            while(!opcion.equals("2") || !opcion.equals("0") || !opcion.equals("1")){
-                opcion = sc.nextLine();
-                switch((opcion)) {
-                    case "1":
-                        System.out.println(" ");
-                        System.out.println("Ingresa una unidad que desea convertir: ");
-                        unidad = sc.nextLine();
-                        System.out.println("Ahora ingresa su valor: ");
-                        valor = sc.nextInt();
-                        Medida.unidadValor(unidad,valor);
-                        break;
-                    case "2":
-                        System.out.println(" ");
-                        System.out.println("Ingrese su valor: ");
-                        valor = sc.nextInt();
-                        Medida.unidadPorDefecto(valor);
-                        sc.nextLine();
-                        break;
-                    case "0":
-                        sc.close();
-                        return;
-                    default:
-                        System.out.println("Opcion no reconocida, porfavor intentelo de nuevo");
-                        System.out.println(" ");
-                }
+            opcion = sc.nextLine();
+            switch ((opcion)) {
+                case "1":
+                    boolean stringValido = false;
+                    while (!stringValido) {
+                        try {
+                            System.out.println("Ingresa una unidad que desea convertir: ");
+                            unidad = sc.nextLine().trim();
+                            Errores.controlStrings(unidad);
+                            System.out.println(" ");
+                            stringValido = true;
+
+                        } catch (Exception e) {
+                            System.out.println("Error: " + e.getMessage());
+                        }
+                    }
+                    boolean doubleValido = false;
+                    while (!doubleValido) {
+                        try{
+                            System.out.println("Ahora ingresa su valor: ");
+                            valor = sc.nextLine().trim();
+                            Medida.unidadValor(unidad, Double.parseDouble(valor));
+                            System.out.println(" ");
+                            doubleValido = true;
+
+                        }catch (Exception e){
+                            System.out.println("Error: Esa opcion no es valida.");
+                        }
+                    }
+                    break;
+                case "2":
+                    System.out.println(" ");
+                    System.out.println("Ingrese su valor: ");
+                    valor = sc.nextLine();
+                    Medida.unidadPorDefecto(Double.parseDouble(valor));
+                    break;
+                case "0":
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Opcion no reconocida, porfavor intentelo de nuevo");
             }
-            
-        }while (true);
+        }
+        System.out.println("Feliz dia");
     }
 }
