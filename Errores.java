c64f25331f7554a1a911f05d8ecfc15382b1b09a
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-22 01:34:59 -0500

    Creacion de la clase errores
    Se separa las responsabilidades de la clase tools y se hace todo el control correspondiente.

---
+++ Errores.java
@@ -0,0 +1,57 @@
+public class Errores {
+    // Manejo de errores de cada categoria
+    // Longitud
+    public static void tipoLongitud(String tipoLongitud) throws Exception{
+        if(tipoLongitud.isEmpty()){
+            throw new Exception("No puede ser vacio");
+        }
+
+        if(!Tools.convertirNuevaUnidad(tipoLongitud).equals("Longitud")){
+            throw new Exception("No se encontro esa unidad, use una de las unidades mencionadas anteriormente.");
+        }
+    }
+    // Masa
+    public static void tipoMasa(String tipoMasa) throws Exception{
+        if(tipoMasa.isEmpty()){
+            throw new Exception("No puede ser vacio");
+        }
+        if(!Tools.convertirNuevaUnidad(tipoMasa).equals("Masa")){
+            throw new Exception("No se encontro esa unidad, use una de las unidades mencionadas anteriormente.");
+        }
+    }
+    // Temperatura
+    public static void tipoTemperatura(String tipoTemperatura) throws Exception{
+        if(tipoTemperatura.isEmpty()){
+            throw new Exception("No puede ser vacio");
+        }
+        if(!Tools.convertirNuevaUnidad(tipoTemperatura).equals("Temperatura")){
+            throw new Exception("No se encontro esa unidad, use una de las unidades mencionadas anteriormente.");
+        }
+    }
+    // Volumen
+    public static void tipoVolumen(String tipoVolumen) throws Exception{
+        if(tipoVolumen.isEmpty()){
+            throw new Exception("No puede ser vacio");
+        }
+        if(!Tools.convertirNuevaUnidad(tipoVolumen).equals("Volumen")){
+            throw new Exception("No se encontro esa unidad, use una de las unidades mencionadas anteriormente.");
+        }
+    }
+    // Tiempo
+    public static void tipoTiempo(String tipoTiempo) throws Exception{
+        if(tipoTiempo.isEmpty()){
+            throw new Exception("No puede ser vacio");
+        }
+        if(!Tools.convertirNuevaUnidad(tipoTiempo).equals("Tiempo")){
+            throw new Exception("No se encontro unidad, use una de las unidades mencionadas anteriormente.");
+        }
+    }
+
+    //Main - control
+    public static void controlStrings(String controlString) throws Exception{
+        if(Tools.convertirNuevaUnidad(controlString).equals(" ")){
+            throw new Exception("Esa opcion no es valida.\n");
+        }
+    }
+
+}
