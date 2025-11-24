c4103d342e29ea535d38c4b7408d667f4bb355d8
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-23 23:05:57 -0500

    Crear clase vault y metodo abrir

---
+++ seccion1/src/main/java/ejercicio2/Vault.java
@@ -0,0 +1,25 @@
+package ejercicio2;
+
+public class Vault {
+    private String metodoSeguridad; // Se usaria PIN, BIOMETRICO, LLAVE FISICA
+    private Object credencial; // La credencial esperada
+
+    public Vault(String metodoSeguridad, Object credencial) {
+        this.metodoSeguridad = metodoSeguridad;
+        this.credencial = credencial;
+    }
+
+    public Boolean abrir(Object credencial) {
+        Boolean valido = false;
+
+        switch (this.metodoSeguridad) {
+            case "PIN", "BIOMETRICO", "LLAVE FISICA":
+                if(this.credencial.equals(credencial)) {
+                    valido = true;
+                }
+                break;
+            default:
+                System.out.println("Metodo incorrecto. Intentalo nuevamente");
+        }return  valido;
+    }
+}
