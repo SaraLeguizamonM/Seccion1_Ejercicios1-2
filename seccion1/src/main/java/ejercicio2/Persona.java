f7c119413deace823d33f33227ab92bed43003da
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-26 08:51:12 -0500

    Comentar en persona

--- seccion1/src/main/java/ejercicio2/Persona.java
+++ seccion1/src/main/java/ejercicio2/Persona.java
@@ -1,24 +1,24 @@
 package ejercicio2;
 
 public class Persona {
-    private final String user;
+    private final String user; 
     private final String password;
     private final Vault vault;
 
-    public Persona(String user, String password, Vault vault) { // Creacion del usuario:D
+    public Persona(String user, String password, Vault vault) { // Constructor de la clase Persona (creacion del usuario:D)
         this.user = user;
         this.password = password;
         this.vault = vault;
     }
 
-    public Vault getvault() {
+    public Vault getvault() { // Nos permite obtener la caja fuerte de la persona
         return this.vault;
     }
 
-    public String getUser() {
+    public String getUser() { // Nos permite obtener el nombre de usuario
         return user;
     }
-    public String getPassword() {
+    public String getPassword() { // Nos permite obtener la contraseña
         return password;
     }
 }
