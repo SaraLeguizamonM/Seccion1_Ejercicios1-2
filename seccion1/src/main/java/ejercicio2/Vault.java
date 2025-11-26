e98481f46a0e3cb8e3eb06a24f0646f3b35f801d
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-26 08:52:21 -0500

    Comentar en vault

--- seccion1/src/main/java/ejercicio2/Vault.java
+++ seccion1/src/main/java/ejercicio2/Vault.java
@@ -4,22 +4,21 @@ public class Vault {
     private String metodoSeguridad; // Se usaria PIN, BIOMETRICO, LLAVE FISICA
     private Object credencial; // La credencial esperada
 
-    public Vault(String metodoSeguridad, Object credencial) {
+    public Vault(String metodoSeguridad, Object credencial) {// Constructor de la clase Vault
         this.metodoSeguridad = metodoSeguridad;
         this.credencial = credencial;
     }
 
-    public Boolean abrir(Object credencial) {
+    public Boolean abrir(Object credencial) {// Metodo para abrir la caja fuerte
         Boolean valido = false;
 
-        switch (this.metodoSeguridad) {
-            case "PIN", "LLAVE FISICA":
+        switch (this.metodoSeguridad) { 
+            case "PIN", "LLAVE FISICA": 
                 try{
-                    if(this.credencial.equals(credencial)) {
+                    if(this.credencial.equals(credencial)) {// Si la credencial coincide
                         valido = true;
                     }
-                    Errores.denegar(this.credencial.toString(), credencial.toString());
-
+                    Errores.denegar(this.credencial.toString(), credencial.toString());// Verificamos si la credencial es correcta
 
                 }catch (Exception e){
                     System.out.println("Error: " + e.getMessage());
@@ -28,12 +27,12 @@ public class Vault {
                 break;
             case "BIOMETRICO":
 
-                if(this.credencial.equals(credencial.toString().toLowerCase())) {
+                if(this.credencial.equals(credencial.toString().toLowerCase())) {// Simulamos el reconocimiento biometrico
                     valido = true;
                 }
                 break;
             default:
-                System.out.println("Metodo incorrecto. Intentalo nuevamente");
+                System.out.println("Metodo incorrecto. Intentalo nuevamente"); // Metodo de seguridad no reconocido
         }return  valido;
     }
 }
