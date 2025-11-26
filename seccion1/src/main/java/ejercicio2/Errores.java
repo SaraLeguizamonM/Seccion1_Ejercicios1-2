79d183b7b73f420e2d14fcf78faef09454777075
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-26 08:49:53 -0500

    Comentar en errores

--- seccion1/src/main/java/ejercicio2/Errores.java
+++ seccion1/src/main/java/ejercicio2/Errores.java
@@ -1,15 +1,15 @@
 package ejercicio2;
 
 public class Errores {
-    public static void denegar(String credencialGuar, String credencialEnv) throws Exception {
-        if(!credencialGuar.equals(credencialEnv)){
+    public static void denegar(String credencialGuar, String credencialEnv) throws Exception { // Metodo para denegar el acceso si la credencial no es correcta
+        if(!credencialGuar.equals(credencialEnv)){// Si las credenciales no coinciden
             throw new Exception("AccesoDenegadoException");
         }
     }
-    public static void controlPIN(String credencial)  throws Exception {
-        if (credencial.trim().isEmpty()){
+    public static void controlPIN(String credencial)  throws Exception {// Metodo para controlar que el pin sea valido
+        if (credencial.trim().isEmpty()){// Si el pin esta vacio
             throw new Exception("El pin no puede estar vacio");
-        } else if (credencial.length() <4) {
+        } else if (credencial.length() <4) {// Si el pin es menor a 4 digitos
             throw new Exception("El pin no puede ser menor de 4 digitos");
         }
     }
